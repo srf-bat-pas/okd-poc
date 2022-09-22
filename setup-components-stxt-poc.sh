865193d2582b21f9809d308da1a82dd2e0f7657c
Claudio <info@gitcd.io>
2022-09-22 16:19:43 +0200

    Finalized argocd and its app-of-apps installation

--- setup-components-stxt-poc.sh
+++ setup-components-stxt-poc.sh
@@ -12,5 +12,11 @@ kubectl apply -f ./k8s-setup/operator-subscriptions/argocd.yaml
 kubectl create secret generic srf-helm-charts-secret --from-file=./srf-helm-chart-private.key
 
 ## Setup ArgoCD
+
 kubectl create namespace argocd
+kubectl apply -f ./k8s-setup/argocd/openshift-argocd-role.yaml
 envsubst < ./k8s-setup/argocd/argocd-server.yaml | kubectl apply -f -
+
+kubectl apply -f ./k8s-setup/argocd/argocd-app-of-apps.yaml
+
+
